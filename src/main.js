8120eab84c7f7e202743062f7949cc31cfb63bff
weidong.he <84313002+L-newbie@users.noreply.github.com>
2026-08-03 12:46:36 +0800

    Integrate PWA setup in main.js
    Added PWA setup function to register service worker.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
+import { setupPwa } from './lib/usePwaUpdate.js'
 
 createApp(App).mount('#app')
+
+// Register the PWA service worker + iOS-safe foreground update re-check.
+setupPwa()
